195dadd109ab14896c34a9e878c2cd5fba7fb143
sschuamcher <seschu@gmail.com>
2020-02-07 18:52:00 -0500

    Updates

---
+++ precache-manifest.3d8e22f6097c985cb9de84bfbfd043b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4317222a153def5694da5c23d8f7b5ab",
+    "url": "/udemy-react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e80db4bf9a8869caff36",
+    "url": "/udemy-react-monsters-rolodex/static/css/main.eaf53fa4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/udemy-react-monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/udemy-react-monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80db4bf9a8869caff36",
+    "url": "/udemy-react-monsters-rolodex/static/js/main.b08ccdd2.chunk.js"
+  },
+  {
+    "revision": "a4bd2a0144aa3646b954",
+    "url": "/udemy-react-monsters-rolodex/static/js/runtime-main.c01a9c01.js"
+  }
+]);
